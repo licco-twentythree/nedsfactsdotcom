695eb0a3790007ffd18a5ca048d2da2475089f04
Ali <ali.bazzi2310@gmail.com>
2024-04-30 23:37:41 +1000

    Added fetch request to get facts from server

--- nedsfactsdotcom-frontend/src/App.jsx
+++ nedsfactsdotcom-frontend/src/App.jsx
@@ -1,7 +1,25 @@
+import { Show, createSignal, onMount } from "solid-js";
 
 function App() {
+
+  const [fact, setFact] = createSignal()
+
+  onMount(async () => { setFact(await getFact())});
+
+  async function getFact() {
+    const res = await fetch('http://localhost:4000/fact/education');
+    return res.json();
+  }
   return (
-    <div></div>
+    <Show 
+    when ={fact() != null}
+    fallback={<div>Loading...</div>}>
+      <div>
+        <div>{fact().summary}</div>
+        <div>{fact().description}</div>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={async () => setFact(await getFact())}>{console.log(fact())}</button>
+      </div>
+    </Show>
   );
 }
 
--- nedsfactsdotcom-server/src/index.mjs
+++ nedsfactsdotcom-server/src/index.mjs
@@ -1,14 +1,16 @@
 import express from 'express';
 import { config } from "dotenv";
 import { ScriptGenerator } from './scriptGenerator.js';
+import cors from 'cors';
 config()
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors())
 
 app.get('/fact/:topic', async (req, res) => {
-    res.send(await ScriptGenerator.generateText(`Generate a fact for this topic: ${req.params.topic}. Return this as a JSON object with two keys: summary (which is a summary of the fact) and description (which is the entire fact)`));
+    res.send(await ScriptGenerator.generateText(`Generate a random interesting fact from this topic: ${req.params.topic}. Return this as a JSON object with two keys: summary (which is a summary of the fact) and description (which is the entire fact)`));
 });
 
 app.listen(port, () =>{
